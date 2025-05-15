384988f13465b4473778c054eb67932838054bf6
dimasprastyoaditama <prastyod647@gmail.com>
2025-05-15 16:36:29 +0700

    void membuat cara untuk menghapus

--- DoubleLinkedList2.cpp
+++ DoubleLinkedList2.cpp
@@ -77,6 +77,15 @@ public:
             current->next = newNode;   // Step 9d: current.next = newNode
         }
 
-        
+        void hapus()
+        {
+            if(START == NULL)
+            {
+                cout << "\nList is empty" << endl;
+                return;
+            }
+
+            
+
 };
 
